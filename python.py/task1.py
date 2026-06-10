5ba31e6a0d571430739592f5a1a7c181f24cefda
karthi271120 <karthikkumar2026@gmail.com>
2026-06-10 14:40:19 +0530

    first commit

---
+++ python.py/task1.py
@@ -0,0 +1,17 @@
+products =[
+           {'id':1,'name':'laptop'},
+           {'id':2,'name':'mobile'},
+           {'id':3,'name':'keyborad'}
+           ]
+name = input('enter the name:')
+found =False
+for a in products:
+    if a ['name'] == name:
+        print('products found')
+        print('id',a['id'])
+        print('name',a['name'])
+        found = True
+        break
+if found == False:
+    print('procut not found')    
+
